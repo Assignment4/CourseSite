768aaf8f45915139f6563b884aa66a7e0e7df9bb
khearn <khearn@uvm.edu>
2014-03-06 14:36:21 -0500

    Practice Navigation Bar

---
+++ practice.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="author" content="Robert M. Erickson">
+<title>Syllabus - CS 142 (A) Advanced Web Design: CSS and Usability</title>
+<meta name="description" content="This course provides an in depth exploration of the foundation of  Cascading Style Sheets and covering Web Usability to help you create a pleasing user experience for visitors to your site."/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- https://www.uvm.edu/~rerickso/education/classes/css/base.css -->
+<!-- https://www.uvm.edu/~khearn/cs142/assignment4/khearn.css -->
+<link href="practice.css" rel="stylesheet" type="text/css" media="screen" />
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+
+
+
+
+
+<body id="syllabus">
+
+<nav id="mainMenu"> <!-- %^%^%^%^%^%^%^%^%^%^%^%^ Navigation %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^% -->
+	<ol>
+		<li class="menuLink"><a href="assignments.php">Assignments</a></li>
+		<li class="menuLink"><a href="calendar.php?abWeek=10">Calendar</a></li>
+		<li class="menuLink"><a href="lectures.php">Lectures</a></li>
+		<li class="menuLink"><a href="media.php">Resources</a></li>
+		<li class="menuLink"><a href="sick.php">Sick</a></li>
+		<li class="menuLink activeLink"><a href="syllabus.php">Syllabus</a></li>
+		<li class="menuLink"><a href="submitAssignment.php">Submit Assignment</a></li>
+	</ol>
+</nav> <!-- navigation -->
+
+</body>
+</html>
+
+
+
+
+
